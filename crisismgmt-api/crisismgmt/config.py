5ed695f3e23c1294b28d1f369a84e2c0ec4ad983
vats16 <piyushvats@gmail.com>
2021-03-22 15:36:49 +1100

    Initialise rds
    initialise and connect flask api to amazon rds

--- crisismgmt-api/crisismgmt/config.py
+++ crisismgmt-api/crisismgmt/config.py
@@ -6,7 +6,7 @@
 
 class BaseConfig(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://admin:tbLs52EC@uts-studio-quantum.cgxjp9m1gyhm.ap-southeast-2.rds.amazonaws.com:3306/quantum'
+    SQLALCHEMY_DATABASE_URI = 'mysql://crisismgmt:crisismgmt.c1e5gxywp9hn.ap-southeast-2.rds.amazonaws.com:3306/crisismgmt'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = "dd02dbe50eb41792067d9d650cd3ba58df0c90c6466ccea7"
